1cd5f86ebfc1fde77cda5386a151257f96147dc5
Anton Khlebnikov <toni.farsh@icloud.com>
2024-11-29 19:46:39 +0500

    git init

---
+++ script.js
@@ -0,0 +1,96 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    startLoad()
+  });
+
+
+///game
+const scrGame = document.querySelector('.screen-game');
+function showGame() {
+    scrGame.classList.remove('hidden');
+}
+function hideGame() {
+    scrGame.classList.add('hidden');
+}
+
+//screen-preloader
+const preloader = document.querySelector('.screen-preloader');
+function showPreloader() {
+    preloader.classList.remove('hidden');
+}
+function hidePreloader() {
+    preloader.classList.add('hidden');
+}
+
+///progress bar 
+const loaderPrecent = document.getElementById('loader-precent');
+const loaderBar = document.querySelector('.loader-bar-progress');
+
+function setProgress(progress) {
+    loaderPrecent.textContent = progress;
+    loaderBar.style.width = `${progress}%`;
+}
+
+function startLoad() {
+    let count = 10;
+    setInterval(()=> {
+        if(count < 100) {
+            setProgress(count);
+        count = count + 7;
+        } else {
+            setProgress(100);
+            preloader.classList.add('start-menu')
+        
+        }
+        
+    },100)
+}
+
+//game screen with box 
+const gameBox = document.querySelector('.game-box');
+function showGameBox() {
+    gameBox.classList.remove('hidden')
+}
+function hideGameBox() {
+    gameBox.classList.add('hidden');
+}
+//game screen with box 
+const gameShot = document.querySelector('.camera-shot');
+function showGameShot() {
+    gameShot.classList.remove('hidden')
+}
+function hideGameShot() {
+    gameShot.classList.add('hidden');
+}
+
+///screenshot-container
+const screenShotScreen = document.querySelector('.screenshot-container');
+function showScreenShot() {
+    screenShotScreen.classList.remove('hidden');
+    showShotBlock();
+    
+}
+function hideScreenShot() {
+    screenShotScreen.classList.add('hidden');
+}
+
+const screenshotBlock = document.querySelector('.screenshot-block');
+function showShotBlock() {
+    screenshotBlock.classList.remove('hidden')
+}
+function hideShotBlock() {
+    screenshotBlock.classList.add('hidden')
+}
+
+///box
+const boxImg = document.querySelector('.box-image')
+function setBoxImg(id) {
+    boxImg.setAttribute('data-img', id)
+}
+
+
+///link 
+function openLinkGoldApple() {
+    window.open('https://goldapple.ru/lp/advent2024');
+}
+
